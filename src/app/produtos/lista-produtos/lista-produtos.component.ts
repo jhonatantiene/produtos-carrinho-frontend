40d3d525f3d226fb94fd78239669a7584dbb233d
jhonatact <jhonatatiene118@gmail.com>
2025-07-14 15:45:31 -0300

    Cabeçalho
    - cabeçalho com 2 botões: Cadastro e Meu carrinho

--- src/app/produtos/lista-produtos/lista-produtos.component.ts
+++ src/app/produtos/lista-produtos/lista-produtos.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 @Component({
   selector: 'app-lista-produtos',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './lista-produtos.component.html',
   styleUrl: './lista-produtos.component.scss'
 })
