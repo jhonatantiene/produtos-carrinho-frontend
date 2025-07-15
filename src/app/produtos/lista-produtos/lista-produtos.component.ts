6fd26bc0438001389ef2b2d891b9dd7b0070d427
jhonatact <jhonatatiene118@gmail.com>
2025-07-15 10:30:07 -0300

    Estrutura html e css do componente lista-produtos
    - Criei a estrutura inicial e estilo inicial da tela lista de produtos

--- src/app/produtos/lista-produtos/lista-produtos.component.ts
+++ src/app/produtos/lista-produtos/lista-produtos.component.ts
@@ -3,12 +3,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 @Component({
   selector: 'app-lista-produtos',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatCardModule, MatGridListModule],
   templateUrl: './lista-produtos.component.html',
   styleUrl: './lista-produtos.component.scss'
 })
 export class ListaProdutosComponent {
 
+  produtos = [ ]
 }
