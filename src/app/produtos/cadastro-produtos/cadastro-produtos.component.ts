bd1c4a5c68763294aff9e3b1d3d8ffca7006c750
jhonatact <jhonatatiene118@gmail.com>
2025-07-16 17:52:30 -0300

    Enviando dados para cadastro de produto

--- src/app/produtos/cadastro-produtos/cadastro-produtos.component.ts
+++ src/app/produtos/cadastro-produtos/cadastro-produtos.component.ts
@@ -7,6 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { ProdutosService } from '../../servicos/produtos.service';
 
 @Component({
   selector: 'app-cadastro-produtos',
@@ -28,23 +29,36 @@ export class CadastroProdutosComponent {
   formulario: FormGroup
   imagemPreview: string | ArrayBuffer | null = null;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private produtosService: ProdutosService) {
     this.formulario = this.fb.group({
       nome: ['', Validators.required],
       descricao: ['', Validators.required],
       preco: [null, [Validators.required, Validators.min(0)]],
-      quantidade: [null, [Validators.required, Validators.min(0)]],
+      quantidade: [null, [Validators.required, Validators.min(1)]],
       imagem: [null]
     });
   }
 
   selecionarImagem(event: Event): void {
-    const file = (event.target as HTMLInputElement).files?.[0];
+    const file = (event.target as HTMLInputElement).files?.[0]
     if (file) {
-      this.formulario.patchValue({ imagem: file });
+      this.formulario.patchValue({ imagem: file.name })
       const reader = new FileReader();
-      reader.onload = () => (this.imagemPreview = reader.result);
-      reader.readAsDataURL(file);
+      reader.onload = () => (this.imagemPreview = reader.result)
+      reader.readAsDataURL(file)
     }
+  }
+
+  salvar() {
+    console.log(this.formulario.value)
+    const produto = this.formulario.value
+    this.produtosService.cadastrar(produto).subscribe({
+      next: () => {
+        console.log('Produto cadastrado com sucesso!')
+      },
+      error: (err) => {
+        console.error('Erro ao cadastrar produto:', err)
+      }
+    })
   }
 }
