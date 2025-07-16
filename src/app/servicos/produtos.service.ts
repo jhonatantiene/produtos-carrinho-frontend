0344752b8e23b65b27633dc59128d3e207770607
jhonatact <jhonatatiene118@gmail.com>
2025-07-16 17:50:43 -0300

    Metodos de buscar e cadastrar produto

--- src/app/servicos/produtos.service.ts
+++ src/app/servicos/produtos.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produto } from '../interfaces/interfaceProdutos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  buscarTodos(): Observable<Produto[]> {
+    return this.http.get<Produto[]>('http://localhost:3000/produtos')
+  }
+
+  cadastrar(produto: Produto) {
+    return this.http.post('http://localhost:3000/produtos/cadastrar', produto)
+  }
 }
+
+
