e5eb337f619120fa09e5eed94e22db13d5b5a59e
jhonatact <jhonatatiene118@gmail.com>
2025-07-16 10:53:31 -0300

    Estrutura base da tela de detalhes do produto
    - Criei a estrutura html e o design inicial da tela

--- src/app/produtos/detalhes-produto/detalhes-produto.component.ts
+++ src/app/produtos/detalhes-produto/detalhes-produto.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-detalhes-produto',
-  imports: [],
+  imports: [MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './detalhes-produto.component.html',
   styleUrl: './detalhes-produto.component.scss'
 })
