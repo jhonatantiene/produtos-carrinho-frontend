e08fee69037b2575c63db17969cae6b96fddb0a8
jhonatantiene <91148527+jhonatantiene@users.noreply.github.com>
2025-07-16 23:01:14 -0300

    Listando os produtos na tela de produtos

--- src/app/produtos/lista-produtos/lista-produtos.component.ts
+++ src/app/produtos/lista-produtos/lista-produtos.component.ts
@@ -1,17 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { ProdutosService } from '../../servicos/produtos.service';
+import { Produto } from '../../interfaces/interfaceProdutos';
 @Component({
   selector: 'app-lista-produtos',
   imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatCardModule, MatGridListModule],
   templateUrl: './lista-produtos.component.html',
   styleUrl: './lista-produtos.component.scss'
 })
-export class ListaProdutosComponent {
+export class ListaProdutosComponent implements OnInit {
 
-  produtos = [ ]
+  constructor(private produtosService: ProdutosService) {}
+
+  produtos: Produto[] = []
+
+  ngOnInit(): void {
+    this.listarProdutos()
+    
+  }
+
+  listarProdutos() {
+    this.produtosService.buscarTodos().subscribe((res) => {
+      this.produtos = res
+      console.log(this.produtos)
+    })
+  }
+
+
 }
