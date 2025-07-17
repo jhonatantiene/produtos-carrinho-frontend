77ec81a01d68511b551a7c035fc1aba4ea5bd456
jhonatact <jhonatatiene118@gmail.com>
2025-07-17 13:06:00 -0300

    Criando o serviço de buscar produto por id

--- src/app/servicos/produtos.service.ts
+++ src/app/servicos/produtos.service.ts
@@ -14,6 +14,10 @@ export class ProdutosService {
     return this.http.get<Produto[]>('https://produtos-api-production.up.railway.app/produtos')
   }
 
+  buscarPorid(id: number | null): Observable<Produto> {
+    return this.http.get<Produto>(`https://produtos-api-production.up.railway.app/produtos/${id}`)
+  }
+
   cadastrar(produto: Produto) {
     return this.http.post('https://produtos-api-production.up.railway.app/produtos/cadastrar', produto)
   }
