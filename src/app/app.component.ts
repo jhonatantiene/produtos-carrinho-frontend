415294b2516901096bfdcf69d22067ee79ed1363
jhonatact <jhonatatiene118@gmail.com>
2025-07-17 12:52:29 -0300

    Confiigurando as rotas para transição suave

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,28 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterOutlet } from "@angular/router";
-
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [
+    trigger('fadeAnimation', [
+      transition('* <=> *', [
+        style({ opacity: 0 }),
+        animate('400ms ease-in-out', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'produtos-carrinho-frontend';
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet?.activatedRouteData?.['animation'];
+  }
 }
+
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,34 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '',
-        loadComponent: () => import('./produtos/lista-produtos/lista-produtos.component').then(m => m.ListaProdutosComponent)
-    },
-
-        {
-        path: 'produtos',
-        loadComponent: () => import('./produtos/lista-produtos/lista-produtos.component').then(m => m.ListaProdutosComponent)
-    },
-
-    {
-        path: 'detalhes',
-        loadComponent: () => import('./produtos/detalhes-produto/detalhes-produto.component').then(m => m.DetalhesProdutoComponent)
-    },
-
-    {
-        path: 'cadastro',
-        loadComponent: () => import('./produtos/cadastro-produtos/cadastro-produtos.component').then(m => m.CadastroProdutosComponent)
-    },
-
-    {
-        path: 'carrinho',
-        loadComponent: () => import('./carrinho/carrinho.component').then(m => m.CarrinhoComponent)
-    }
+  {
+    path: '',
+    loadComponent: () =>
+      import('./produtos/lista-produtos/lista-produtos.component').then(m => m.ListaProdutosComponent),
+    data: { animation: 'Home' }
+  },
+  {
+    path: 'produtos',
+    loadComponent: () =>
+      import('./produtos/lista-produtos/lista-produtos.component').then(m => m.ListaProdutosComponent),
+    data: { animation: 'Produtos' }
+  },
+  {
+    path: 'detalhes/:id',
+    loadComponent: () =>
+      import('./produtos/detalhes-produto/detalhes-produto.component').then(m => m.DetalhesProdutoComponent),
+    data: { animation: 'Detalhes' }
+  },
+  {
+    path: 'cadastro',
+    loadComponent: () =>
+      import('./produtos/cadastro-produtos/cadastro-produtos.component').then(m => m.CadastroProdutosComponent),
+    data: { animation: 'Cadastro' }
+  },
+  {
+    path: 'carrinho',
+    loadComponent: () =>
+      import('./carrinho/carrinho.component').then(m => m.CarrinhoComponent),
+    data: { animation: 'Carrinho' }
+  }
 ];
